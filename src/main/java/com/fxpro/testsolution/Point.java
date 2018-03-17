a19c45799006b38f651cea89be99d5b7047c1992
DmytroNiehrii <Dmytro.Niehrii@playtech.com>
2018-03-17 19:58:35 +0200

    Dirrection added

---
+++ src/main/java/com/fxpro/testsolution/Point.java
@@ -0,0 +1,9 @@
+package com.fxpro.testsolution;
+
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
+public class Point {
+    public int i, j;
+
+}
--- src/main/java/com/fxpro/testsolution/WaterDrop.java
+++ src/main/java/com/fxpro/testsolution/WaterDrop.java
@@ -1,10 +1,14 @@
 package com.fxpro.testsolution;
 
+import com.fxpro.testsolution.enums.Dirrection;
 import com.fxpro.testsolution.enums.WaterDropFlowResult;
 import com.fxpro.testsolution.enums.WaterDropMovingResult;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Supplier;
 
+import static com.fxpro.testsolution.enums.Dirrection.Down;
 import static com.fxpro.testsolution.enums.TerrainType.Air;
 import static com.fxpro.testsolution.enums.TerrainType.Water;
 import static com.fxpro.testsolution.enums.WaterDropFlowResult.Missed;
@@ -17,7 +21,9 @@ public class WaterDrop implements Supplier<WaterDropFlowResult> {
     private final int terrainPosition;
     private final int topIndex;
     private final Landscape landscape;
-    private int currentI, currentJ;
+    private Point currentPoint = new Point();
+    private Set<Point> track = new HashSet<>();
+    private Dirrection currentDirrection = Down;
 
     public WaterDrop(Landscape landscape, int terrainPosition) {
         this.landscape = landscape;
@@ -38,7 +44,7 @@ public class WaterDrop implements Supplier<WaterDropFlowResult> {
 
     private WaterDropFlowResult startFlow() {
         landscape.printTerrain();
-        landscape.print("===> WaterDrop.startFlow: " + currentI + ", " + currentJ + "\n");
+        landscape.print("===> WaterDrop.startFlow: " + currentPoint.i + ", " + currentPoint.j + "\n");
         while (true) {
             landscape.printTerrain();
             WaterDropMovingResult movingResult = moveDown();
@@ -48,30 +54,36 @@ public class WaterDrop implements Supplier<WaterDropFlowResult> {
                 movingResult = moveRight();
                 if (movingResult == Leak) return Missed;
                 if (movingResult == CanNotMove) {
-                    return Stored;
+
+
+                    movingResult = moveLeft(); // left
+                    if (movingResult == Leak) return Missed; // left
+                    if (movingResult == CanNotMove)  // left
+                        return Stored;
                 }
             }
+            if (isLooped()) return Stored;
         }
     }
 
     private WaterDropMovingResult moveRight() {
-        return move(currentI + 1, currentJ);
+        return move(currentPoint.i + 1, currentPoint.j);
     }
 
     private WaterDropMovingResult moveLeft() {
-        return move(currentI - 1, currentJ);
+        return move(currentPoint.i - 1, currentPoint.j);
     }
 
     private WaterDropMovingResult moveDown() {
-        return move(currentI, currentJ - 1);
+        return move(currentPoint.i, currentPoint.j - 1);
     }
 
     private WaterDropMovingResult move(int i, int j) {
-        if (currentI - 1 < 0 ||
-            currentI + 1 >= landscape.getLandscapeWidth() ||
-            currentJ - 1 < 0)
+        if (currentPoint.i - 1 < 0 ||
+            currentPoint.i + 1 >= landscape.getLandscapeWidth() ||
+            currentPoint.j - 1 < 0)
         {
-            landscape.setTerrainType(currentI, currentJ, Air);
+            landscape.setTerrainType(currentPoint.i, currentPoint.j, Air);
             return Leak;
         }
 
@@ -79,20 +91,24 @@ public class WaterDrop implements Supplier<WaterDropFlowResult> {
         if (landscape.getTerrainType(i, j) != Air)
             return CanNotMove;
         else {
-            landscape.changeCells(currentI, currentJ, i, j);
-            currentI = i;
-            currentJ = j;
+            landscape.changeCells(currentPoint.i, currentPoint.j, i, j);
+            currentPoint.i = i;
+            currentPoint.j = j;
             return Moved;
         }
     }
 
     private void putWaterDropOnTerrain(int i, int j) {
         landscape.setTerrainType(i, j, Water);
-        currentI = i;
-        currentJ = j;
+        currentPoint.i = i;
+        currentPoint.j = j;
     }
 
-    private Landscape getLandscaзe() {
-        return landscape;
+    public boolean isLooped() {
+        if (track.contains(currentPoint)) return true;
+        else {
+            track.add(currentPoint);
+            return false;
+        }
     }
 }
